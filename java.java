cedaccd3357c49ba08eac9a876a6f102e445a2b6
lakshyaasonii <lakshyasoni2023@gmail.com>
2025-08-16 12:53:30 +0530

    lakshya

--- java.java
+++ java.java
@@ -1,16 +1,16 @@
 import java.util.*;
 public class java{
-   
-
- public static void main(String args[]){
-    Scanner sc = new Scanner(System.in);
-    System.out.print("Enter the number of elements present in array: ");
-    int n = sc.nextInt();
-    int marks[] = new int[n];
-    for(int i=0;i<n;i++){
-        marks[i] = sc.nextInt();
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int marks[][][] = new int[2][2][2];
+        for(int i=0;i<2;i++){
+            for(int j=0;j<2;j++){
+               for(int k=0;k<2;k++){  
+                marks[i][j][k] = sc.nextInt();
+               }
+            }
+        }
+        sc.close();
+        System.out.print(marks[1][1][1]);
     }
-    System.out.print(Arrays.toString(marks));
-    
- }
 }
