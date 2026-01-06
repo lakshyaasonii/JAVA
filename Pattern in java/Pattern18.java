1c6e35c7422609563d40dd0a1579988590502ab5
lakshyaasonii <lakshyasoni2023@gmail.com>
2026-01-06 22:54:46 +0530

    solve more problems

---
+++ Pattern in java/Pattern18.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Pattern18 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter size of the pattern: ");
+        int n = sc.nextInt();
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=n;j++){
+                if(j==i || j==(n+1 -i)){
+                    System.out.print("*");
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
