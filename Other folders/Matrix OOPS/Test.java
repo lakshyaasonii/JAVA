3ac3f409a94f53129648c6790c208934ba502ba9
lakshyaasonii <lakshyasoni2023@gmail.com>
2026-01-09 00:52:43 +0530

    implement OOPS concepts in matrix questions

---
+++ Other folders/Matrix OOPS/Test.java
@@ -0,0 +1,77 @@
+import java.util.Scanner;
+
+class Matrix{
+    int m,n;
+    int matrix[][];
+    Matrix(int m, int n){
+        this.m = m;
+        this.n = n;
+         matrix = new int[m][n];
+    }
+    void input(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter elements of the matrix: ");
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j] = sc.nextInt();
+            }
+            System.out.println();
+        }
+        sc.close();
+    }
+    void display(){
+        System.out.println("MATRIX is: ");
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+    void trace(){
+        if(m != n){
+            System.out.println("Matrix is not square..");
+        }
+        else{
+            int sum = 0;
+            for(int i=0; i<m; i++){
+                    sum = sum + matrix[i][i];
+            }
+            System.out.println("Sum of diagonal element is " + sum);
+        }
+    }
+    void border(){
+        System.out.println("Border element of the matrix..");
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(i==0 || i==(m-1) || j==0 || j==(n-1)){
+                    System.out.print(matrix[i][j] + " ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    void sum(){
+        int sum = 0;
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                sum = sum + matrix[i][j];
+            }
+        }
+        System.out.println("Sum of all elements of the matrix" + sum);
+    }
+}
+    
+public class Test {
+    public static void main(String args[]){
+        Matrix m1 = new Matrix(3,3);
+        m1.input();
+        m1.display();
+        m1.trace();
+        m1.border();
+        m1.sum();
+    }
+}
